5dc221d5f36fd8dba8694c7d45cbef424dbe3fa8
RodrigoDuarte31 <rodrigoduarte31@gmail.com>
2024-08-29 17:34:41 -0300

    data added

--- capstone_project.R
+++ capstone_project.R
@@ -4,24 +4,22 @@ y = c(1, 3, 6, 2, 7, 4, 5)
 
 result = cor(x, y, method = "pearson")
 
-cat("Pearson correlation coefficient is:", result)
+cat("Pearson correlation coefficient is: ", result)
 
+# More info
 result = cor.test(x, y, method = "pearson")
-
-print(result)
+result
 
 # First try with real data from TCC
-data = read.csv("comparison_th5.csv")
+data = read.csv("th25.csv")
 
 x = data$automatic
 y = data$manual
 
 result = cor.test(x, y, method = "pearson")
-
-print(result)
+result
 
 # First try with plots
-
 library(ggplot2)
 
 # Scatter plot with correlation coefficient
@@ -36,64 +34,41 @@ ggplot(data = data, aes(x = automatic, y = manual)) +
   theme_minimal()
 
 # Doing some data processing...
-data2 = read.csv("comparison_th375.csv")
-data3 = read.csv("comparison_th025.csv")
+rownames(data) <- data$video
+colnames(data)[colnames(data) == "automatic"] <- "auto25p"
 
-# Rename column
-colnames(data)[colnames(data) == "automatic"] <- "auto5p"
+data2 = read.csv("th375.csv")
+data3 = read.csv("th25.csv")
+
 colnames(data2)[colnames(data2) == "automatic"] <- "auto375p"
-colnames(data3)[colnames(data3) == "automatic"] <- "auto25p"
+colnames(data3)[colnames(data3) == "automatic"] <- "auto5p"
 
 data <- merge(x = data, y = data2, by = c("video", "manual"))
 data <- merge(x = data, y = data3, by = c("video", "manual"))
 
-rownames(data) <- data$video
 data <- subset(data, select = -video)
+
+# Data distribution
+boxplot(data, xlab = "Observer", ylab = "Duration (s)")
 
 # Trying a multiple plot
 par(mfrow=c(3,1))
-plot(auto5p~manual, data=data)
-plot(auto375p~manual, data=data)
 plot(auto25p~manual, data=data)
-
+plot(auto375p~manual, data=data)
+plot(auto5p~manual, data=data)
 dev.off()
 
+# Correlation matrix
 corrmat <- cor(data)
 corrmat
 
-library(ggcorrplot)
-
-ggcorrplot(corrmat, type = "lower", lab = TRUE)
-
+# Trying different forms of visualization
 library("ellipse")
-
 plotcorr(corrmat, mar = c(1, 1, 1, 1))
+
+library(ggcorrplot)
+ggcorrplot(corrmat, type = "lower", lab = TRUE)
+ggcorrplot(corrmat, method = "circle", type = "lower", lab = TRUE)
 
 library("GGally")
 ggpairs(data)
-
-relu <- function(x){
-  return(pmax(0,x))
-}
-
-x <- seq(-2, 2, length.out = 41)
-
-relu_values <- relu(x)
-
-ggplot(data.frame(x = x, y = relu_values), aes(x = x, y = y)) +
-  geom_line(size = 1.5, color = "blue") +
-  labs(
-    title = "Função ReLU",
-    x = "x",
-    y = "ReLU(x)"
-  ) +
-  theme_minimal()
-
-ggplot(data) + 
-  geom_boxplot(fill="slateblue", alpha=0.2) + 
-  xlab("cyl")
-
-boxplot(data, xlab = "Observer", ylab = "Duration (s)")
-
-pearsonmat <- cor.test(data$manual, data$auto25p)
-print(pearsonmat)
